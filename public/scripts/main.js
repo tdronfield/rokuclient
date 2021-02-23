3501c2e0925bf3f4fe38fc9a3d876c52aa7b4793
tdronfield <you@example.com>
2021-02-23 14:53:26 -0500

    adding HBS and vue

---
+++ public/scripts/main.js
@@ -0,0 +1,29 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
+import TheMovieThumb from './components/TheMovieThumbnailComponent.js';
+
+(() => {
+    const vm = new Vue({
+        data: {
+            allMovies: []
+        },
+
+        created: function(){
+            // a fetch call to grab all our movies from the database
+            fetch("/api/movies")
+                .then(res => res.json())
+                .then(data => {
+                    console.table(data);
+                    this.allMovies = data;
+                })
+                .catch(err => console.error(err));
+        },
+
+        methods: {
+
+        },
+
+        components: {
+            moviethumb: TheMovieThumb
+        }
+    }).$mount("#app");
+})();
