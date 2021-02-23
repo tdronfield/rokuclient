90a7acd90fe728e01bc8a60f50a1bc4c9bf9685d
tdronfield <you@example.com>
2021-02-23 13:34:30 -0500

    adding proxy and file structure

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const hbs = require('hbs');
 
 const app = express();
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5000;
 
 // Tell HBS where our views content lives,
 // Like how we tell routes where to find index
-app.set('views', path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "public"));
 // Use the hbs engine to render everything in views
-app.set('views', 'hbs');
+app.set("views", "hbs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
