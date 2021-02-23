6bbca6b844d85463e5e97c07afeb56bdc958baa4
tdronfield <you@example.com>
2021-02-23 12:41:12 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+
+const port = process.env.PORT || 5050;
+
+// Tell HBS where our views content lives,
+// Like how we tell routes where to find index
+app.set('views', path.join(__dirname, "public"));
+// Use the hbs engine to render everything in views
+app.set('views', 'hbs');
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/index"));
+
+app.listen(port, () => {
+    console.log(`app is running on ${port}`);
+})
